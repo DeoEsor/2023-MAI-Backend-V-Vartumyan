ce676459dc948166674d77459947cc2d941bdc5c
DeoEsor <RulianValeriy@yandex.ru>
2023-03-06 18:34:48 +0300

    added LRUCache implementation for services

---
+++ src/Services/CatalogService/DAL/Repositories/Interfaces/LRUCache.cs
@@ -0,0 +1,27 @@
+﻿namespace CatalogService.DAL.Repositories.Interfaces;
+
+public interface ILruCache<in TKey, TValue>
+{
+    /// <summary>
+    /// Gets the value associated with the specified key.
+    /// </summary>
+    /// <param name="key">The key of the value to get.</param>
+    /// <param name="value">When this method returns, contains the value associated with
+    /// the specified key, if the key is found; otherwise, the default value for the 
+    /// type of the <paramref name="value" /> parameter.</param>
+    /// <returns>true if contains an element with the specified key; otherwise, false.</returns>
+    bool TryGet(TKey key, out TValue? value);
+
+    /// <summary>
+    /// Adds the specified key and value to the cache.
+    /// </summary>
+    /// <param name="key">The key of the element to add.</param>
+    /// <param name="value">The value of the element to add.</param>
+    void Add(TKey key, TValue value);
+    
+    /// <summary>
+    /// Removes from cache value by specified key
+    /// </summary>
+    /// <param name="key">The key of the element to remove</param>
+    void Remove(TKey key);
+}
---
+++ src/Services/IdentityService/DAL/Repositories/Models/LruCache.EntryModel.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable once CheckNamespace
+namespace IdentityService.DAL.Repositories;
+
+public sealed partial class LruCache<TKey, TValue> where TKey : notnull
+{
+    private struct Entry
+    {
+        public readonly LinkedListNode<TKey>? Node;
+        public TValue Value;
+
+        public Entry(LinkedListNode<TKey>? node, TValue value)
+        {
+            Node = node;
+            Value = value;
+        }
+    }   
+}
